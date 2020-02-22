578bfebe167b327003b4558845c5cd58982a1ca4
Зінченко Людмила Вікторівна <lu.zin.kpi@gmail.com>
2020-02-22 23:56:05 +0200

    add ApplicationContext

---
+++ CleanFix/Context/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using CleanFix.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanFix.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=ec2-54-246-90-10.eu-west-1.compute.amazonaws.com;Port=5432;Database=d5uoraj48dtav1;Username=wwwrnjjrkfwrwk;Password=d4bdda56ce85d8e33f6c46b1341cb444a481218aa2c778289191b4810f990a7d");
+        }
+    }
+}
--- CleanFix/Models/Project.cs
+++ CleanFix/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace CleanFix.Models
 {
     public class Project
     {
-
+        [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
@@ -17,5 +18,18 @@ namespace CleanFix.Models
         public string Category { get; set; }
 
         public string MainPhoto { get; set; }
+
+        public decimal Budget { get; set; }
+        
+        public bool Status { get; set; }
+
+        public string District { get; set; }
+
+        public string Location { get; set;  }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
     }
 }
