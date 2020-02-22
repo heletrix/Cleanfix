9198e1d3ed9ef648bf677b0037f93d4138716aab
Зінченко Людмила Вікторівна <lu.zin.kpi@gmail.com>
2020-02-22 23:04:15 +0200

    add folder Models

---
+++ CleanFix/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanFix.Models
+{
+    public class Project
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Decription { get; set; }
+
+        public string Category { get; set; }
+
+        public string MainPhoto { get; set; }
+    }
+}
